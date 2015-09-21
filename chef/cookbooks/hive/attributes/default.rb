b739a1338ba9775590ba4dd061f67960b3b0ccd1
tanakahiroki <tnkhrk.122@gmail.com>
2015-09-22 00:45:05 +0900

    refs #13 modify chef hadoop user add rb

--- chef/cookbooks/hive/attributes/default.rb
+++ chef/cookbooks/hive/attributes/default.rb
@@ -1,2 +1,3 @@
 default["hadoop"]["user"]["name"] = "hadoop"
 default["hadoop"]["user"]["password"] = "$1$VDVO.uIp$ni5JxiOLS2rLZPl.tlY50."
+default["s3"]["root_dir"] = "s3://hirokitanaka-stock";
--- chef/cookbooks/hive/recipes/adduser.rb
+++ chef/cookbooks/hive/recipes/adduser.rb
@@ -6,14 +6,16 @@ user "#{node.hadoop.user.name}" do
   action :create
 end
 
+# add him to wheel group and let him be a sodoer.
+bash "ec2-user" do
+  code "sudo sh -c \"%echo \\\"%wheel ALL=(ALL) NOPASSWD: ALL\\\" >> /etc/sudoers\""
+end
 
-# add him to wheel group in order to use sudo.
 group "wheel" do
   action [:modify]
   members ["#{node.hadoop.user.name}"]
   append true
 end
-
 
 # ssh settings
 directory "/home/#{node.hadoop.user.name}/.ssh" do
@@ -23,10 +25,18 @@ directory "/home/#{node.hadoop.user.name}/.ssh" do
   action :create
 end
 
+bash "#{node.hadoop.user.name}" do
+  code "aws s3 cp #{node.s3.root_dir}/pems/id_rsa ~/home/#{node.hadoop.user.name}/id_rsa"
+end
 
-cookbook_file "/home/#{node.hadoop.user.name}/.ssh/authorized_keys" do
-  source "#{node.hadoop.user.name}/authorized_keys"
-  owner "#{node.hadoop.user.name}"
-  group "wheel"
-  mode 0600
+bash "#{node.hadoop.user.name}" do
+  code "aws s3 cp #{node.s3.root_dir}/pems/id_rsa.pub ~/home/#{node.hadoop.user.name}/id_rsa.pub"
+end
+
+bash "#{node.hadoop.user.name}" do
+  code "cat ~/home/#{node.hadoop.user.name}/id_rsa.pub >> authorized_keys"
+end
+
+bash "#{node.hadoop.user.name}" do
+  code "chmod 600 ~/home/#{node.hadoop.user.name}/*"
 end
