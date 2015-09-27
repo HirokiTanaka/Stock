c77f6a878f39e6719913b432207a3350b4400e81
tanakahiroki <tnkhrk.122@gmail.com>
2015-09-28 02:58:50 +0900

    refs #13 add install hadoop and hive recipes.

--- chef/cookbooks/hive/recipes/hive.rb
+++ chef/cookbooks/hive/recipes/hive.rb
@@ -1,33 +1,33 @@
 # install hive
 execute "wget hive" do
   cwd "/tmp"
-  command "wget http://ftp.tsukuba.wide.ad.jp/software/apache/hive/hive-#{node.hive.version}/apache-hive-#{node.hive.version}-bin.tar.gz -O hive-#{node.hive.version}.tar.gz"
+  command "wget http://ftp.tsukuba.wide.ad.jp/software/apache/hive/hive-#{node.hive.version}/apache-hive-#{node.hive.version}-bin.tar.gz"
   user "#{node.hadoop.user.name}"
   group "wheel"
   action :run
   notifies :run, "execute[tar hive]", :immediately
-  not_if { File.exists?("#{node.hive.install_dir}/hive-#{node.hive.version}") }
+  not_if { File.exists?("#{node.hive.install_dir}/apache-hive-#{node.hive.version}-bin") }
 end
 
 execute "tar hive" do
   cwd "/tmp"
-  command "tar zxf hive-#{node.hive.version}.tar.gz"
+  command "tar zxf apache-hive-#{node.hive.version}-bin.tar.gz"
   user "#{node.hadoop.user.name}"
   group "wheel"
   action :run
   notifies :run, "execute[move hive]", :immediately
-  not_if { File.exists?("#{node.hive.install_dir}/hive-#{node.hive.version}") }
+  not_if { File.exists?("#{node.hive.install_dir}/apache-hive-#{node.hive.version}-bin") }
 end
 
 execute "move hive" do
   cwd "/tmp"
   command <<-_EOF_
-      mv hive-#{node.hive.version} #{node.hive.install_dir}
-      ln -s #{node.hive.install_dir}/hive-#{node.hive.version} #{node.hive.install_dir}/hive
+      mv apache-hive-#{node.hive.version}-bin #{node.hive.install_dir}
+      ln -s #{node.hive.install_dir}/apache-hive-#{node.hive.version}-bin #{node.hive.install_dir}/hive
       chown -R #{node.hadoop.user.name}:wheel #{node.hive.install_dir}/
   _EOF_
   user "root"
   group "root"
   action :run
-  not_if { File.exists?("#{node.hive.install_dir}/hive-#{node.hive.version}") }
+  not_if { File.exists?("#{node.hive.install_dir}/apache-hive-#{node.hive.version}-bin") }
 end
