e2d1485e0bef2a039337099601dd12d183c1502c
tanakahiroki <tnkhrk.122@gmail.com>
2015-10-04 15:33:45 +0900

    refs #13 modify hadoop env

--- chef/cookbooks/hive/templates/default/hadoop-env.sh
+++ chef/cookbooks/hive/templates/default/hadoop-env.sh
@@ -3,7 +3,11 @@ export JAVA_HOME=/usr/java/latest
 
 # hadoop
 export HADOOP_INSTALL=<%= node.hadoop.install_dir %>/hadoop
+export HADOOP_HOME=${HADOOP_INSTALL}
 export HADOOP_CLASSPATH=${HADOOP_CLASSPATH}:${HADOOP_INSTALL}/share/hadoop/tools/lib/*
 
 # hive
 export HIVE_HOME=<%= node.hive.install_dir %>/hive
+
+# set up path
+export PATH=${PATH}:${HADOOP_HOME}/bin:${HIVE_HOME}/bin
